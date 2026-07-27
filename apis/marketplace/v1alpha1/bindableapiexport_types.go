9a1d7363800ccfb1fa4db8c6eb4e5e98959a65ac
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-07-27 11:32:21 +0200

    Add PermissionClaims
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- apis/marketplace/v1alpha1/bindableapiexport_types.go
+++ apis/marketplace/v1alpha1/bindableapiexport_types.go
@@ -27,12 +27,24 @@ type BindableAPIExportSpec struct {
 	IdentityHash string `json:"identityHash,omitempty"`
 	// Resources are the resources exported by the APIExport.
 	Resources []BindableResource `json:"resources,omitempty"`
+	// PermissionClaims are the permission claims the APIExport requests.
+	PermissionClaims []BindablePermissionClaim `json:"permissionClaims,omitempty"`
 }
 
 // BindableResource is a single resource offered by a BindableAPIExport.
 type BindableResource struct {
 	Group    string `json:"group,omitempty"`
 	Resource string `json:"resource,omitempty"`
+}
+
+// BindablePermissionClaim is a permission claim requested by a BindableAPIExport.
+type BindablePermissionClaim struct {
+	Group    string `json:"group,omitempty"`
+	Resource string `json:"resource,omitempty"`
+	// Verbs are the API verbs the claim covers.
+	Verbs []string `json:"verbs,omitempty"`
+	// IdentityHash is the APIExport identity the claimed schema belongs to; empty for core types.
+	IdentityHash string `json:"identityHash,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
